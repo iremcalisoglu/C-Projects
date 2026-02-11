b7c074bddfba98087f65eab679706055dd456b16
iremcalisoglu <irem145367@gmail.com>
2026-02-11 12:44:14 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number, cube;
+            number = Convert.ToDouble(textBox1.Text);
+            cube = number * number * number;
+
+            MessageBox.Show("Cube of number: " + cube);
+            label1.Text = Convert.ToString(cube);
+        }
+    }
+}
